514816982dea39c01e6bbd59f3654bfc190d289f
Skandini06nandan <62279852+Skandini06nandan@users.noreply.github.com>
2021-06-11 15:04:39 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,11 @@
+class atm:
+    def __init__(cardNumber, pin):
+        self.cardNumber = cardNumber
+        self.pin = pin
+      
+
+    def balanceEnquiry():
+        print("Total Balance: 1,30,000/-")
+
+    def CashWithdrawl():
+        print ("Enter your amount below:")
